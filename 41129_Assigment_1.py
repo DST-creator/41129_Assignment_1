5e55d7f0b8d8dbb7f154c32cbbf9f6044d63bfb8
Davis Stöwer <davis.stoewer@gmail.com>
2024-09-27 15:38:38 +0200

    Added .pgf export to all figures

--- 41129_Assigment_1.py
+++ 41129_Assigment_1.py
@@ -25,12 +25,12 @@ mpl.rcParams['axes.prop_cycle'] = mpl.cycler('color', ['k', 'k', 'k', 'k'])
 #                                 + mpl.cycler('linestyle', ['-', '--', '-.', ':'])
 
 #Text sizes
-mpl.rcParams['font.size'] = 18
-mpl.rcParams['xtick.labelsize'] = 18
-mpl.rcParams['ytick.labelsize'] = 18
-mpl.rcParams['axes.labelsize'] = 20
-mpl.rcParams['axes.titlesize'] = 25
-mpl.rcParams['legend.fontsize'] = 20
+mpl.rcParams['font.size'] = 20
+mpl.rcParams['xtick.labelsize'] = 20
+mpl.rcParams['ytick.labelsize'] = 20
+mpl.rcParams['axes.labelsize'] = 25
+mpl.rcParams['axes.titlesize'] = 30
+mpl.rcParams['legend.fontsize'] = 25
 
 #Padding
 mpl.rcParams['figure.subplot.top'] = .94    #Distance between suptitle and subplots
@@ -57,15 +57,15 @@ mpl.rcParams['savefig.bbox'] = "tight"
 
 #%% Input data
 
-replot_tasks = dict(T1=False, 
-                    T4=False,
-                    T5=False,
-                    T6=False,
+replot_tasks = dict(T1=True, 
+                    T4=True,
+                    T5=True,
+                    T6=True,
                     T7=True,
-                    T8=False,
-                    T9=False,
-                    T10=False,
-                    T11=False,
+                    T8=True,
+                    T9=True,
+                    T10=True,
+                    T11=True,
                     )
 
 h = .07         #[m]
@@ -99,11 +99,14 @@ if replot_tasks["T1"]:
     
     #Formatting
     ax1.set_title('Mean velocity')
-    ax1.set_ylabel('\[y\:[m]\]')
-    ax1.set_xlabel(r'\[\overline{u}\:[m/s]\]')
+    ax1.set_ylabel('$y\:[m]$')
+    ax1.set_xlabel(r'$\overline{u}\:[m/s]$')
     ax1.grid(zorder=1)
     
-    fig1.savefig(fname="Task_1_plot.svg")
+    fname = "Task_1_plot"
+    fig1.savefig(fname=fname+".svg")
+    fig1.savefig(fname+".pdf", format="pdf")       # Save PDF for inclusion
+    fig1.savefig(fname+".pgf")                     # Save PGF file for text inclusion in LaTeX
     plt.close(fig1)
 else:
     print("Plot for Task 1 not replotted")
@@ -141,19 +144,23 @@ func_log_layer = lambda y: popt[0]*np.log(y) + popt[1]
 
 if replot_tasks["T4"]:
     fig4, ax4 = plt.subplots(figsize=(10, 10))
-    ax4.scatter(u_means, y, label = "true data", s=100, zorder=2)
+    ax4.scatter(u_means, y, label = "Measurements", s=100, zorder=2)
     ax4.plot(func_log_layer(y[i_log]),
             y[i_log],
             label="Approximation function",
             ls="-", c='k', zorder=2)
     
     #Formatting
-    ax4.set_ylabel('\[y\:[m]\]')
-    ax4.set_xlabel(r'\[\overline{u}\:[m/s]\]')
+    ax4.set_ylabel('$y\:[m]$')
+    ax4.set_xlabel(r'$\overline{u}\:[m/s]$')
     ax4.set_yscale("log")
     ax4.grid(zorder=1)
     ax4.legend(loc="lower right")
-    fig4.savefig(fname="Task_4_plot.svg")
+    
+    fname = "Task_4_plot"
+    fig4.savefig(fname=fname+".svg")
+    fig4.savefig(fname+".pdf", format="pdf")       # Save PDF for inclusion
+    fig4.savefig(fname+".pgf")                     # Save PGF file for text inclusion in LaTeX
     plt.close(fig4)
 else:
     print("Plot for Task 4 not replotted")
@@ -166,7 +173,7 @@ y_plus = y*U_f/nu
 
 if replot_tasks["T5"]:
     fig5, ax5 = plt.subplots(figsize=(16, 10))
-    plt5_sc1 = ax5.scatter(u_means/U_f, y_plus, label = "True data", 
+    plt5_sc1 = ax5.scatter(u_means/U_f, y_plus, label = "Measurements", 
                            s=170, linewidths=1.8, zorder=2)
     plt5_line1 = ax5.plot(func_log_layer(y[i_log])/U_f,
                           y_plus[i_log],
@@ -179,17 +186,21 @@ if replot_tasks["T5"]:
     
     #Regions
     rect_visc_sub = mpl.patches.Rectangle((-10,0), 40, 5, 
-                                          hatch="//\\\\", fc = "1", alpha = .32,
+                                          hatch="-\\", fc = "1", alpha = .32,
+                                          ec="k", lw=1,
                                           label= "Viscous sublayer", zorder=0)
     rect_visc_buffer = mpl.patches.Rectangle((-10,5), 40, 30-5, 
-                                             hatch="//", fc = "1", alpha = .42,
+                                             hatch="/", fc = "1", alpha = .42,
+                                             ec="k", lw=1,
                                              label= "Buffer layer", zorder=0)
     rect_log = mpl.patches.Rectangle((-10,30), 40, .1*h*U_f_est/nu-30, 
-                                     hatch="\\\\/",  fc = "1", alpha = .32,
+                                     hatch="\\/",  fc = "1", alpha = .32,
+                                     ec="k", lw=1,
                                      label= "Logarithmic layer", zorder=0)
     rect_out = mpl.patches.Rectangle((-10, .1*h*U_f_est/nu), 
                                      40, .9*h*U_f_est/nu, 
-                                     hatch="\\\\", fc = "1", alpha = .42,
+                                     hatch="\\", fc = "1", alpha = .42,
+                                     ec="k", lw=1,
                                      label= "Outer region", zorder=0)
     
     ax5.add_patch(rect_visc_sub)
@@ -204,14 +215,17 @@ if replot_tasks["T5"]:
     
     #Formatting
     dx_ticks = 2
-    ax5.set_ylabel('\[y^+\:[-]\]')
-    ax5.set_xlabel(r'\[\frac{\overline{u}}{U_f}\:[-]\]')
+    ax5.set_ylabel('$y^+$')
+    ax5.set_xlabel(r'$\frac{\overline{u}}{U_f}$')
     ax5.set_yscale("log")
     ax5.set_xlim([0, max(u_means/U_f)*1.05])
     ax5.set_xticks(np.arange(0, int(np.ceil(u_means[-1]/U_f/dx_ticks))*dx_ticks, 
                             dx_ticks))
     ax5.grid(zorder=1)
-    fig5.savefig(fname="Task_5_plot.svg")
+    fname = "Task_5_plot"
+    fig5.savefig(fname=fname+".svg")
+    fig5.savefig(fname+".pdf", format="pdf")       # Save PDF for inclusion
+    fig5.savefig(fname+".pgf")                     # Save PGF file for text inclusion in LaTeX
     
     if not replot_tasks["T6"]:
         plt.close(fig5)
@@ -235,15 +249,18 @@ u_vD = integrate.cumulative_trapezoid (y=dfunc_3_108(y_plus, kappa=.4, A_d=25),
 
 if replot_tasks["T6"]:
     if replot_tasks["T5"]:
-        plt6_sc1 = ax5.scatter(u_vD/U_f,
+        plt6_line1 = ax5.plot(u_vD/U_f,
                                y_plus[1:],
                                label="van Driest velocity", 
-                               marker = "d", s=100, zorder=2)
-        plt5_lgd1 = plt.legend(handles=[plt5_sc1, plt5_line1[0], plt6_sc1], 
+                               marker = "d", ls="--", c='k', lw=1.5, zorder=2)
+        plt5_lgd1 = plt.legend(handles=[plt5_sc1, plt5_line1[0], plt6_line1[0]], 
                                loc='upper left')
         plt5_lgd1 = ax5.add_artist(plt5_lgd1)
-        fig5.savefig(fname="Task_6_plot.svg",
-                    bbox_inches = "tight")
+        
+        fname = "Task_6_plot"
+        fig5.savefig(fname=fname+".svg")
+        fig5.savefig(fname+".pdf", format="pdf")       # Save PDF for inclusion
+        fig5.savefig(fname+".pgf")                     # Save PGF file for text inclusion in LaTeX
         plt.close(fig5)
     else:
         print("Task 6 can only be replotted if Task 5 is plotted")
@@ -292,29 +309,51 @@ if replot_tasks["T7"]:
                 label=r"$\frac{\sqrt{-\overline{u^{\prime}v^{\prime}}}}{U_f}$",
                 marker = "v", s=100, zorder=2)
     
-    #Empty scatter (needed for the distribution of the labels in the columns 
-    # of the legend)
-    ax7.scatter(1,1, label=' ', c='#ffffff') 
+# =============================================================================
+#     #Empty scatter (needed for the distribution of the labels in the columns 
+#     # of the legend)
+#     ax7.scatter(1,1, label=' ', c='#ffffff') 
+#     
+#     #Regions
+#     rect_visc_sub = mpl.patches.Rectangle((0,0), 5, 5, 
+#                                           hatch="-\\", fc = "1", alpha = .32,
+#                                           ec="k", lw=1,
+#                                           label= "Viscous sublayer", zorder=0)
+#     rect_visc_buffer = mpl.patches.Rectangle((5, 0), 30-5, 5, 
+#                                              hatch="/", fc = "1", alpha = .42,
+#                                              ec="k", lw=1,
+#                                              label= "Buffer layer")
+#     rect_log = mpl.patches.Rectangle((30,0), .1*h*U_f/nu-30, 5, 
+#                                      hatch="\\/",  fc = "1", alpha = .32,
+#                                      ec="k", lw=1,
+#                                      label= "Logarithmic layer", zorder=0)
+#     rect_out = mpl.patches.Rectangle((.1*h*U_f/nu, 0), 
+#                                      .9*h*U_f/nu, 5, 
+#                                      hatch="\\", fc = "1", alpha = .42,
+#                                      ec="k", lw=1,
+#                                      label= "Outer region", zorder=0)
+#      
+#     ax7.add_patch(rect_visc_sub)
+#     ax7.add_patch(rect_visc_buffer)
+#     ax7.add_patch(rect_log)
+#     ax7.add_patch(rect_out)
+# =============================================================================
     
-    #Regions
-    rect_visc_sub = mpl.patches.Rectangle((0,0), 5, 5, 
-                                          hatch="//\\\\", fc = "1", alpha = .32,
-                                          label= "Viscous sublayer", zorder=0)
-    rect_visc_buffer = mpl.patches.Rectangle((5, 0), 30-5, 5, 
-                                             hatch="//", fc = "1", alpha = .42,
-                                             label= "Buffer layer")
-    rect_log = mpl.patches.Rectangle((30,0), .1*h*U_f/nu-30, 5, 
-                                     hatch="\\\\/",  fc = "1", alpha = .32,
-                                     label= "Logarithmic layer", zorder=0)
-    rect_out = mpl.patches.Rectangle((.1*h*U_f/nu, 0), 
-                                     .9*h*U_f/nu, 5, 
-                                     hatch="\\\\", fc = "1", alpha = .42,
-                                     label= "Outer region", zorder=0)
-     
-    ax7.add_patch(rect_visc_sub)
-    ax7.add_patch(rect_visc_buffer)
-    ax7.add_patch(rect_log)
-    ax7.add_patch(rect_out)
+    #Region boundaries
+    ax7.axvline(5, ls="--", c="k", lw=1.5)
+    ax7.axvline(30, ls="--", c="k", lw=1.5)
+    ax7.axvline(.1*h*U_f/nu, ls="--", c="k", lw=1.5)
+    
+    #Annotations for regions
+    arrowstyle = dict(arrowstyle="->", 
+                      connectionstyle="angle,angleA=0,angleB=90")
+    ax7.text(0,4.9, " ") #To make space for annotations
+    ax7.annotate("Viscous sublayer", (2.5,4.5), (5,4.85), arrowprops = arrowstyle)
+    ax7.annotate("Buffer layer", (0,4.5), (12,4.6))
+    ax7.annotate("Logarithmic layer", (0,4.5), (55,4.6))
+    ax7.annotate("Outer region", (.1*h*U_f/nu+3, 4.5), (.1*h*U_f/nu+3-15,4.85), 
+                 arrowprops = arrowstyle)
+    
     
     #Formatting
     ax7.set_xlim([0,100])
@@ -322,11 +361,12 @@ if replot_tasks["T7"]:
     ax7.set_xlabel('$y^+$')
     ax7.set_ylabel(r'Turbulence quantities')
     ax7.grid(zorder=1)
-    ax7.legend(loc="upper right", ncols=2)
+    ax7.legend(loc="upper right", framealpha=1)
     
-    fig7.savefig(fname="Task_7_plot.svg")
-    fig7.savefig("Task_7_plot.pdf", format="pdf")       # Save PDF for inclusion
-    fig7.savefig("Task_7_plot.pgf")                     # Save PGF file for text inclusion in LaTeX
+    fname = "Task_7_plot"
+    fig7.savefig(fname=fname+".svg")
+    fig7.savefig(fname+".pdf", format="pdf")       # Save PDF for inclusion
+    fig7.savefig(fname+".pgf")                     # Save PGF file for text inclusion in LaTeX
     
     plt.close(fig7)
 else:
@@ -351,12 +391,15 @@ if replot_tasks["T8"]:
     
     #Formatting
     ax8.set_xlim([0,1])
-    ax8.set_xlabel('$y/h\:[-]$')
-    ax8.set_ylabel(r'Turbulence quantities $[-]$')
+    ax8.set_xlabel('$y/h$')
+    ax8.set_ylabel(r'Turbulence quantities')
     ax8.grid(zorder=1)
     ax8.legend(loc="upper right")
-    fig8.savefig(fname="Task_8_plot.svg",
-                bbox_inches = "tight")
+    
+    fname = "Task_8_plot"
+    fig8.savefig(fname=fname+".svg")
+    fig8.savefig(fname+".pdf", format="pdf")       # Save PDF for inclusion
+    fig8.savefig(fname+".pgf")                     # Save PGF file for text inclusion in LaTeX
     plt.close(fig8)
 else:
     print("Plot for Task 8 not replotted")
@@ -374,10 +417,14 @@ if replot_tasks["T9"]:
     
     #Formatting
     ax9.set_xlim([0,1])
-    ax9.set_xlabel('\[y/h\:[-]\]')
-    ax9.set_ylabel(r'\[k/U_f^2\:[-]\]')
+    ax9.set_xlabel('$y/h$')
+    ax9.set_ylabel(r'$\frac{k}{U_f^2}$')
     ax9.grid(zorder=1)
-    fig9.savefig(fname="Task_9_plot.svg")
+    
+    fname = "Task_9_plot"
+    fig9.savefig(fname=fname+".svg")
+    fig9.savefig(fname+".pdf", format="pdf")       # Save PDF for inclusion
+    fig9.savefig(fname+".pgf")                     # Save PGF file for text inclusion in LaTeX
     plt.close(fig9)
 else:
     print("Plot for Task 9 not replotted")
@@ -405,11 +452,15 @@ if replot_tasks["T10"]:
     
     #Formatting
     ax10.set_xlim([0,1])
-    ax10.set_xlabel('\[y/h\:[-]\]')
-    ax10.set_ylabel(r"\[\frac{-\rho\overline{u^{'}v^{'}}}{U_f^2}\:\:[-]\]")
+    ax10.set_xlabel('$y/h$')
+    ax10.set_ylabel(r"$\frac{-\rho\overline{u^{\prime}v^{\prime}}}{U_f^2}$")
     ax10.grid(zorder=1)
     ax10.legend(loc="upper right")
-    fig10.savefig(fname="Task_10_plot.svg")
+    
+    fname = "Task_10_plot"
+    fig10.savefig(fname=fname+".svg")
+    fig10.savefig(fname+".pdf", format="pdf")       # Save PDF for inclusion
+    fig10.savefig(fname+".pgf")                     # Save PGF file for text inclusion in LaTeX
     plt.close(fig10)
 else:
     print("Plot for Task 10 not replotted")
@@ -422,11 +473,15 @@ if replot_tasks["T11"]:
     
     #Formatting
     ax11.set_xlim([0,1])
-    ax11.set_xlabel('\[y/h\:[-]\]')
-    ax11.set_ylabel(r"\[-\rho\overline{u^{'}v^{'}} "
-                    r"\cdot \frac{\partial\overline{u}}{\partial y}\:\:[-]\]")
+    ax11.set_xlabel('$y/h$')
+    ax11.set_ylabel(r"$-\rho\overline{u^{\prime}v^{\prime}}"
+                    r"\cdot \frac{\partial\overline{u}}{\partial y}$")
     ax11.grid(zorder=1)
-    fig11.savefig(fname="Task_11_plot.svg")
+    
+    fname = "Task_11_plot"
+    fig11.savefig(fname=fname+".svg")
+    fig11.savefig(fname+".pdf", format="pdf")       # Save PDF for inclusion
+    fig11.savefig(fname+".pgf")                     # Save PGF file for text inclusion in LaTeX
     plt.close(fig11)
 else:
     print("Plot for Task 11 not replotted")
